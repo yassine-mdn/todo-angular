139fc29e30ddd7aa21a8f97d70461838cf7ece8e
yassine <yassinemouddene@gmail.com>
2024-05-07 16:11:07 +0100

    feat: input field component

---
+++ src/app/shared/components/input-field/input-field.component.ts
@@ -0,0 +1,32 @@
+import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {FormControl, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIcon} from "@ng-icons/core";
+
+@Component({
+  selector: 'app-input-field',
+  standalone: true,
+  imports: [
+    FormsModule,
+    NgIcon,
+    ReactiveFormsModule
+  ],
+  templateUrl: './input-field.component.html',
+  styleUrl: './input-field.component.css'
+})
+export class InputFieldComponent {
+
+  @Input() label: string = '';
+  @Input() type: string = 'text';
+  @Output() valueChange = new EventEmitter<string>();
+
+  control: FormControl = new FormControl('', Validators.required);
+
+  get value(): string {
+    return this.control.value;
+  }
+
+  set value(val: string) {
+    this.control.setValue(val);
+    this.valueChange.emit(val);
+  }
+}
